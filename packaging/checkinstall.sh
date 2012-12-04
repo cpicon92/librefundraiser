0c6490cca99c4085c98c57347d6faeb79b8f6187
Kristian Picon <christian.picon@mail.mcgill.ca>
2012-12-04 10:59:08 -0500

    changes to packaging

--- packaging/checkinstall.sh
+++ packaging/checkinstall.sh
@@ -1,1 +1,1 @@
-checkinstall --maintainer 'kristianpicon@gmail.com' --pkggroup database --provides 'librefundraiser' --requires 'java-runtime'
+checkinstall --maintainer 'kristianpicon@gmail.com' --pkggroup database --provides 'librefundraiser' --requires 'java-runtime' --arch 'all'
