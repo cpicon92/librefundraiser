c10233d36e1ab3e4e269c4e4bb686784da432562
Kristian Picon <christian.picon@mail.mcgill.ca>
2012-12-26 20:22:06 -0500

    added temporary gui tester

---
+++ src/net/sf/librefundraiser/gui/Tester.java
@@ -0,0 +1,34 @@
+package net.sf.librefundraiser.gui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class Tester extends Composite {
+
+	/**
+	 * Create the composite.
+	 * @param parent
+	 * @param style
+	 */
+	public Tester(Composite parent, int style) {
+		super(parent, style);
+		setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		DateTime dateTime = new DateTime(this, SWT.BORDER | SWT.CALENDAR);
+		dateTime.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+}
