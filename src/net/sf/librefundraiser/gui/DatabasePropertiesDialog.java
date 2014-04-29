a4c92c2d35153d407638bbf2af16c641d10e440c
Kristian Picon <kristian.picon@webcbg.com>
2014-04-29 16:56:52 -0400

    start working on custom fields

--- src/net/sf/librefundraiser/gui/DatabasePropertiesDialog.java
+++ src/net/sf/librefundraiser/gui/DatabasePropertiesDialog.java
@@ -11,7 +11,6 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -19,8 +18,10 @@ import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 
 public class DatabasePropertiesDialog extends Dialog {
@@ -112,35 +113,48 @@ public class DatabasePropertiesDialog extends Dialog {
 		lblCustomFieldExplanation.setText("LibreFundraiser allows an unlimited number of custom fields for your donors. When you add a field here, it will show up on the \"Custom\" tab of the donor entry. ");
 		
 		//TODO implement add/remove custom field functionality
-		List list = new List(grpCustomFields, SWT.BORDER | SWT.V_SCROLL);
+		Table list = new Table(grpCustomFields, SWT.BORDER);
+		list.setHeaderVisible(true);
 		list.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		list.setItems(new String[] {"Race", "Creed", "Colour", "Sex", "Gender", "Sexual Orientation"});
+		
+		TableColumn tblclmnName = new TableColumn(list, SWT.NONE);
+		tblclmnName.setWidth(100);
+		tblclmnName.setText("Name");
+		
+		TableColumn tblclmnType = new TableColumn(list, SWT.NONE);
+		tblclmnType.setWidth(100);
+		tblclmnType.setText("Type");
+		
+		String[][] items = {{"Race", "Text"}, {"Creed", "Text"}, {"Colour", "Choice"}, {"Sex", "Choice"}, {"Gender", "Number"}, {"Sexual Orientation", "Boolean"}};
+		
+		for (String[] item : items) {
+			TableItem tableItem = new TableItem(list, SWT.NONE);
+			tableItem.setText(item);
+		}
+
 		
 		Composite compositeCustomFieldButtons = new Composite(grpCustomFields, SWT.NONE);
 		compositeCustomFieldButtons.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		GridLayout gl_compositeCustomFieldButtons = new GridLayout(2, false);
+		GridLayout gl_compositeCustomFieldButtons = new GridLayout(3, false);
 		gl_compositeCustomFieldButtons.marginWidth = 0;
 		gl_compositeCustomFieldButtons.marginHeight = 0;
 		compositeCustomFieldButtons.setLayout(gl_compositeCustomFieldButtons);
 		
 		Button btnDeleteField = new Button(compositeCustomFieldButtons, SWT.NONE);
-		GridData gd_btnDeleteField = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_btnDeleteField.widthHint = 150;
-		btnDeleteField.setLayoutData(gd_btnDeleteField);
 		btnDeleteField.setSize(126, 27);
-		btnDeleteField.setText("Delete selected field");
+		btnDeleteField.setText("Delete selected");
 		
 		Button btnAddAField = new Button(compositeCustomFieldButtons, SWT.NONE);
 		btnAddAField.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		btnAddAField.setText("Add a field");
+		btnAddAField.setText("Add new");	
+		
+		Button btnEdit = new Button(compositeCustomFieldButtons, SWT.NONE);
+		btnEdit.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		btnEdit.setText("Edit");
 		
 		Composite compositeButtons = new Composite(shell, SWT.NONE);
+		compositeButtons.setLayout(new RowLayout(SWT.HORIZONTAL));
 		compositeButtons.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-		RowLayout rl_compositeButtons = new RowLayout(SWT.HORIZONTAL);
-		rl_compositeButtons.marginRight = 0;
-		rl_compositeButtons.marginLeft = 0;
-		rl_compositeButtons.pack = false;
-		compositeButtons.setLayout(rl_compositeButtons);
 		
 		Button btnOk = new Button(compositeButtons, SWT.NONE);
 		btnOk.addSelectionListener(new SelectionAdapter() {
@@ -150,7 +164,6 @@ public class DatabasePropertiesDialog extends Dialog {
 				shell.close();
 			}
 		});
-		btnOk.setLayoutData(new RowData(80, SWT.DEFAULT));
 		btnOk.setText("OK");
 		
 		Button btnCancel = new Button(compositeButtons, SWT.NONE);
