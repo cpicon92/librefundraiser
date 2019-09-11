4241df1b2a3694e8e58f4a1faa0d48671e6785d4
Kristian Picon <kristian@waffl.in>
2019-09-11 09:41:20 -0400

    increment version number

--- packaging/checkinstall.sh
+++ packaging/checkinstall.sh
@@ -1,1 +1,1 @@
-checkinstall --pkgname librefundraiser --pkgversion 0.0.7 --pkggroup database --provides 'librefundraiser' --requires '' --arch 'all' --maintainer 'kristianpicon@gmail.com' 
+checkinstall --pkgname librefundraiser --pkgversion 0.0.8 --pkggroup database --provides 'librefundraiser' --requires '' --arch 'all' --maintainer 'kristianpicon@gmail.com' 
